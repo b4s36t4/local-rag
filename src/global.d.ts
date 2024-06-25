f701794cbce9a09664acfc18dd334b93ceaa623a
Mahesh <maheshmahi19@outlook.com>
2024-06-25 17:55:21 +0530

    add modal support to the component

--- src/global.d.ts
+++ src/global.d.ts
@@ -13,4 +13,9 @@ declare interface Window {
   MODEL_LOADED: boolean;
   CHAT_PDF: boolean;
   db: import("voy-search").Voy;
+  modal: HTMLDivElement & {
+    open: () => void;
+    close: () => void;
+    enabled: boolean;
+  };
 }
